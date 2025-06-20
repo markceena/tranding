94e04dedcaee649825d4a06b3bf69a1434c0d2f3
markceena <markceena3@gmail.com>
2025-06-20 12:17:25 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 
-project = 'IOLO'
+project = 'Trend Micro Guide'
 copyright = '2025'
-author = 'Jane Waton'
+author = 'June Laxi'
 
 release = '1.0'
 
